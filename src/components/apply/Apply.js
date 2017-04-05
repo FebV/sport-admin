e39305326b3b899f44ea8a5abefa2780c7e335dc
FebV <cn.zhaoyifan@gmail.com>
2017-04-05 14:53:54 +0800

    add static pages

--- src/components/apply/Apply.js
+++ src/components/apply/Apply.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import DatePicker from 'material-ui/DatePicker';
+
 export default class Apply extends React.Component {
     constructor(props) {
         super(props);
@@ -7,7 +16,121 @@ export default class Apply extends React.Component {
 
     render() {
         return (
-            <div>Apply</div>
+            <MuiThemeProvider>
+            <Tabs>
+                <Tab label="校内申请">
+                <MuiThemeProvider>
+                <div style={{margin: "40px 0", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <Paper style={{width: "40%", padding: "20px"}}>
+                 <SelectField
+                    floatingLabelText="选择校区"
+                    value="123"
+                    >
+                    <MenuItem value={1} primaryText="Never" />
+                    <MenuItem value={2} primaryText="Every Night" />
+                    <MenuItem value={3} primaryText="Weeknights" />
+                    <MenuItem value={4} primaryText="Weekends" />
+                    <MenuItem value={5} primaryText="Weekly" />
+                </SelectField><br />
+                <SelectField
+                    floatingLabelText="选择场馆"
+                    value="123"
+                    >
+                    <MenuItem value={1} primaryText="Never" />
+                    <MenuItem value={2} primaryText="Every Night" />
+                    <MenuItem value={3} primaryText="Weeknights" />
+                    <MenuItem value={4} primaryText="Weekends" />
+                    <MenuItem value={5} primaryText="Weekly" />
+                </SelectField><br />
+                <DatePicker
+                    style={{display: "inline-block"}}
+                    hintText="使用日期"
+                    DateTimeFormat={Intl.DateTimeFormat}
+                    locale="zh-CN"
+                    cancelLabel="取消"
+                    okLabel="确定"
+                /><br />
+                <TextField 
+                    floatingLabelText="使用学院"
+                /><br />
+                <TextField 
+                    floatingLabelText="活动内容"
+                /><br />
+                <TextField 
+                    floatingLabelText="参加人数"
+                /><br />
+                <TextField 
+                    floatingLabelText="负责人姓名"
+                /><br />
+                <TextField 
+                    floatingLabelText="联系方式"
+                /><br />
+                <TextField 
+                    floatingLabelText="备注"
+                /><br />
+                <RaisedButton
+                    label="提交"
+                />
+                </Paper>
+                </div>
+                </MuiThemeProvider>
+                </Tab>
+                <Tab label="校外申请" >
+                <MuiThemeProvider>
+                <div style={{margin: "40px 0", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <Paper style={{width: "40%", padding: "20px"}}>
+                 <SelectField
+                    floatingLabelText="选择校区"
+                    value="123"
+                    >
+                    <MenuItem value={1} primaryText="Never" />
+                    <MenuItem value={2} primaryText="Every Night" />
+                    <MenuItem value={3} primaryText="Weeknights" />
+                    <MenuItem value={4} primaryText="Weekends" />
+                    <MenuItem value={5} primaryText="Weekly" />
+                </SelectField><br />
+                <SelectField
+                    floatingLabelText="选择场馆"
+                    value="123"
+                    >
+                    <MenuItem value={1} primaryText="Never" />
+                    <MenuItem value={2} primaryText="Every Night" />
+                    <MenuItem value={3} primaryText="Weeknights" />
+                    <MenuItem value={4} primaryText="Weekends" />
+                    <MenuItem value={5} primaryText="Weekly" />
+                </SelectField><br />
+                <DatePicker
+                    style={{display: "inline-block"}}
+                    hintText="使用日期"
+                    DateTimeFormat={Intl.DateTimeFormat}
+                    locale="zh-CN"
+                    cancelLabel="取消"
+                    okLabel="确定"
+                /><br />
+                <TextField 
+                    floatingLabelText="使用单位"
+                /><br />
+                <TextField 
+                    floatingLabelText="活动内容"
+                /><br />
+                <TextField 
+                    floatingLabelText="负责人姓名"
+                /><br />
+                <TextField 
+                    floatingLabelText="联系方式"
+                /><br />
+                <TextField 
+                    floatingLabelText="备注"
+                /><br />
+                <RaisedButton
+                    label="提交"
+                />
+                </Paper>
+                </div>
+                </MuiThemeProvider>
+                </Tab>
+            </Tabs>
+            </MuiThemeProvider>
         )
     }
 }
--- src/components/query/Query.js
+++ src/components/query/Query.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import DatePicker from 'material-ui/DatePicker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn} from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
 
 export default class Query extends React.Component {
     constructor(props) {
@@ -9,9 +11,10 @@ export default class Query extends React.Component {
 
     render() {
         return (
+            <div style={{marginTop: "20px"}}>
             <MuiThemeProvider>
-            <div>
-                <span>从</span>
+            <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                <span style={{margin: "0 20px"}}>从：</span><br />
                 <DatePicker
                     style={{display: "inline-block"}}
                     hintText="起始日期"
@@ -19,8 +22,8 @@ export default class Query extends React.Component {
                     locale="zh-CN"
                     cancelLabel="取消"
                     okLabel="确定"
-                />
-                <span>至</span>                
+                /><br />
+                <span style={{margin: "0 20px"}}>至：</span><br />
                 <DatePicker
                     style={{display: "inline-block"}}
                     hintText="截止日期"
@@ -31,6 +34,29 @@ export default class Query extends React.Component {
                 />
             </div>
             </MuiThemeProvider>
+
+            <div style={{marginTop: "20px", display: "flex", width: "100%", justifyContent: "center", alignItems: "center"}}>
+            <MuiThemeProvider>
+            <Paper style={{width: "80%"}}>
+            <Table>
+            <TableHeader
+                displaySelectAll={false}
+                adjustForCheckbox={false}
+            >
+                <TableRow>
+                <TableHeaderColumn>返</TableHeaderColumn>
+                <TableHeaderColumn>回</TableHeaderColumn>
+                <TableHeaderColumn>结</TableHeaderColumn>
+                <TableHeaderColumn>果</TableHeaderColumn>
+                </TableRow>
+            </TableHeader>
+            <TableBody>
+            </TableBody>
+            </Table>
+            </Paper>
+            </MuiThemeProvider>
+            </div>
+            </div>
         )
     }
 }
